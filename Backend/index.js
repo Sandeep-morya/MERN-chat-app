5ce5e79d7fe1dfd1b0b0696133c1aa1cb973cf30
Sandeep-morya <saabmaurya@gmail.com>
2023-02-25 23:13:46 +0530

    project setup done

---
+++ Backend/index.js
@@ -0,0 +1,18 @@
+﻿require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const { PORT } = process.env;
+const app = express();
+
+/* Cross-origin-resource-sharing */
+app.use(cors());
+
+app.get("/", (req, res) => {
+	res.send({ message: "successfull" });
+});
+
+/* Listenin the Port */
+app.listen(PORT, () => {
+	console.log("sever is running");
+});
